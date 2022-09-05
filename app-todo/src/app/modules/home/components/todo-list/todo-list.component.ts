53198c064a38b08a95a110174a52a12bd4301bc1
Jader Raniere Albrez <jader.ralbrez@gmail.com>
2022-09-05 16:13:57 -0300

    feat: Adicionado botões de delete e adicionar

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    {task: "Minha nova tarefa", checked: true},
+    {task: "Minha nova tarefa 2", checked: false},
+  ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm=window.confirm("Você deseja limpar todas as tarefas?")
+
+    if(confirm){
+      this.taskList = [];
+    }
   }
 
 }
